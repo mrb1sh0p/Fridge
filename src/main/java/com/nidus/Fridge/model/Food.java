f722bdd68642eaab77c7188ceac01bc8f28f0da7
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-03-12 17:33:17 -0300

    Make Model
    Feito o model para a aplicação

--- src/main/java/com/nidus/Fridge/model/Food.java
+++ src/main/java/com/nidus/Fridge/model/Food.java
@@ -1,5 +1,68 @@
 package com.nidus.Fridge.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDate;
+
+/**
+ * This is a model class for food.
+ * 
+ * @author João Gustavo Soares Bispo
+ * @date 2025-03-12
+ * @version 1.0.0
+ */
+@Entity
+@Table(name = "food_table")
 public class Food {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate expirationDate;
+    private Integer quantity;
+
+    public Food() {
+    }
+
+    public Food(String name, LocalDate expirationDate, Integer quantity) {
+        this.name = name;
+        this.expirationDate = expirationDate;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
